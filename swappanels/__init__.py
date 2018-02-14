db5acbe67383b2afed7d82f7028d256f10d10d8a
Uzo Olisemeka <uzo@designbymobius.ca>
2018-02-14 12:51:00 -0500

    maintain pane selections after swap

--- swappanels/__init__.py
+++ swappanels/__init__.py
@@ -3,7 +3,22 @@ from fman import DirectoryPaneCommand
 class SwapPanel(DirectoryPaneCommand):
     def __call__(self):
         panes = self.pane.window.get_panes()
-        rpane = panes[0].get_path()
-        lpane = panes[1].get_path()
-        panes[0].set_path(lpane)
-        panes[1].set_path(rpane)
+
+        lpane = panes[0]
+        lpane_path = lpane.get_path()
+        lpane_selection = lpane.get_selected_files()
+
+        rpane = panes[1]
+        rpane_path = rpane.get_path()
+        rpane_selection = rpane.get_selected_files()
+
+        lpane.set_path( rpane_path )
+        rpane.set_path( lpane_path )
+
+        if( lpane_selection ):
+            for url in lpane_selection:
+                rpane.toggle_selection( url )
+
+        if( rpane_selection ):
+            for url in rpane_selection:
+                lpane.toggle_selection( url )
